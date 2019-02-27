e6ab6d6661274d9c0395377416faeeb1da020263
mt5225 <mt5225@gmail.com>
2019-02-26 19:38:20 -0800

    add file monitor

--- xml_monitor.py
+++ xml_monitor.py
@@ -1,7 +1,12 @@
+'''
+curl -X POST -H 'Content-type: text/xml' -d @req.xml http:/localhost:7800
+'''
 import time
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+import requests
 
+_DIX_END_POINT = '''http://localhost:7800'''
 
 class Watcher:
     DIRECTORY_TO_WATCH = "/tmp/fetched"
@@ -22,9 +27,7 @@ class Watcher:
 
         self.observer.join()
 
-
 class Handler(FileSystemEventHandler):
-
     @staticmethod
     def on_any_event(event):
         if event.is_directory:
@@ -33,11 +36,20 @@ class Handler(FileSystemEventHandler):
         elif event.event_type == 'created':
             # Take any action here when a file is first created.
             print "Received created event - %s." % event.src_path
+            post_xml(event.src_path)
 
-        elif event.event_type == 'modified':
-            # Taken any action here when a file is modified.
-            print "Received modified event - %s." % event.src_path
+        # elif event.event_type == 'modified':
+        #     # Taken any action here when a file is modified.
+        #     print "Received modified event - %s." % event.src_path
 
+def post_xml(xml_file):
+    headers = {'Content-Type':'text/xml'}
+
+    # Open the XML file.
+    with open(xml_file) as xml:
+        # Give the object representing the XML file to requests.post.
+        r = requests.post(_DIX_END_POINT, data=xml, headers=headers)
+        print (r.content)
 
 if __name__ == '__main__':
     w = Watcher()
